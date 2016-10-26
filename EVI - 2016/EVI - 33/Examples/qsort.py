dd3f52a6d5b252e111e2244f08748b3f0c349331
Miguel Angel Astor Romero <sonofgrendel@gmail.com>
2016-10-26 16:30:18 -0400

    Update qsort.py

--- EVI - 2016/EVI - 33/Examples/qsort.py
+++ EVI - 2016/EVI - 33/Examples/qsort.py
@@ -10,7 +10,7 @@ def qsort(l):
         more = qsort([x for x in l[1:] if x > l[0]])
         return less + [l[0]] + more
 
-a = [r.randint(0, 100) for x in xrange(100)]
+a = [r.randint(0, 100) for x in range(100)]
 b = qsort(a)
 print a
 print b
